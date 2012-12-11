98e4da8fc68e8e7289f54d3f8e9d8957eb4de9cc
abelass <websolutions@mychacra.net>
2012-12-11 08:04:17 +0100

    fichiers de langue

--- lang/edition_pub_en.php
+++ lang/edition_pub_en.php
@@ -7,22 +7,22 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// E
 	'edition_anonyme' => 'Enable anonymous edition',
-	'edition_pub_titre' => 'Édition publique',
-	'explication_edition_anonyme' => 'If enabled no name or emaol must be given, otherwise the author must be connected or give his name and email address.',
+	'edition_pub_titre' => 'Public eedition',
+	'explication_edition_anonyme' => 'If enabled, no name or email must is required, otherwise the author must be connected or give his name and email address.',
 	'explication_login' => 'To be able to further manage your contributions, you need to login first. <br/>Please <a rel="nofollow" class="login_modal" href="@url@" title="@titre_login@">click here</a> to login or create an account<br/>
 	This is not mandatory!',
 
 	// C
-	'cfg_titre_parametrages' => 'Paramétrages',
+	'cfg_titre_parametrages' => 'Settings',
 
 	// O
-	'objets_edition_pub' => 'Les objets propos&eacute; pour l\'&eacute;dition publique',
+	'objets_edition_pub' => 'Object prposed for public editing',
 	
 	//S
-	'statut_defaut' => 'Statut d&eacute;faut pour',	
+	'statut_defaut' => 'Default status for',	
 
 	// T
-	'titre_page_configurer_edition_pub' => 'Configure Édition Publique',
+	'titre_page_configurer_edition_pub' => 'Configure Public Edition',
 );
 
 ?>
